ee1755246d01cb31067dd98356474fd42ee54c71
Joseph Fujimoto <j0sephmalakie@gmail.com>
2025-11-06 03:19:21 -0800

    Getting rid of src files and starting over

--- fnd-agent-local/src/config/env.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const env = {
-  PORT: Number(process.env.PORT || 8080),
-  NODE_ENV: process.env.NODE_ENV || "development",
-  ORCH_URL: process.env.ORCH_URL || "http://localhost:7001/chat",
-};
